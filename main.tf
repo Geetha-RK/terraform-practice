a9526492a108911dec5f6ce004332fac7590ba89
Geetha Rashinkar Kumar <geetha.kumar@ravl.io>
2025-07-18 16:26:14 -0400

    Added S3 KMS key generation

--- main.tf
+++ main.tf
@@ -88,6 +88,30 @@ resource "aws_s3_object" "test_upload_bucket" {
 
 }
 
-#KMS Keys and encryption onto S3 bucket - important to keep the content secure
+#KMS Keys and encryption onto S3 bucket - important to keep the content secure - generate kms key to server side encrytion
+
+resource "aws_kms_key" "s3_bucket_kms_key" {
+    description = "KMS key for s3 bucket"
+    deletion_window_in_days = 7
+    tags = {
+        name = "KMS Key for S3 bucket"
+    }
+}
+# Creating alias name forthe kms key
+resource "aws_kms_alias" "s3_bucket_kms_key_alias" {
+    name = "alias/s3_bucket_kms_key_alias"
+    target_key_id = aws_kms_key.s3_bucket_kms_key.key_id
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_bucket_encryption_with_kms_key" {
+  bucket = aws_s3_bucket.test_bucket.id
+  
+  rule{
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.s3_bucket_kms_key.arn
+      sse_algorithm = "aws:kms"
+    }
+  }
+}
 
 
